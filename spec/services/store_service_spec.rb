72ad1bda933eb2ce989974e4820ac6f114405c9f
wratterman <will.ratterman@gmail.com>
2017-09-13 10:32:25 -0600

    StoreService tested

--- spec/services/store_service_spec.rb
+++ spec/services/store_service_spec.rb
@@ -26,5 +26,30 @@ describe StoreService do
       expect(raw_store).to have_key(:phone)
       expect(raw_store[:phone]).to be_a(String)
     end
+
+    it "returns the stores within 25 miles of different zip" do
+      zip = "90210"
+
+      raw_stores = StoreService.get_nearby_stores(zip)
+      raw_store  = raw_stores.first
+
+      expect(raw_stores.count).to eq(20)
+      expect(raw_stores).to be_a(Array)
+
+      expect(raw_store).to have_key(:longName)
+      expect(raw_store[:longName]).to be_a(String)
+
+      expect(raw_store).to have_key(:city)
+      expect(raw_store[:city]).to be_a(String)
+
+      expect(raw_store).to have_key(:distance)
+      expect(raw_store[:distance]).to be_a(Float)
+
+      expect(raw_store).to have_key(:storeType)
+      expect(raw_store[:storeType]).to be_a(String)
+
+      expect(raw_store).to have_key(:phone)
+      expect(raw_store[:phone]).to be_a(String)
+    end
   end
 end
