14f62fddb5d25ff2cff751eaa5f5facc85713791
wratterman <will.ratterman@gmail.com>
2017-09-13 10:48:22 -0600

    test suite passing for user story 2(feature, service, poro) and still from story 1. looking into pagination

--- spec/models/store_spec.rb
+++ spec/models/store_spec.rb
@@ -18,5 +18,22 @@ describe Store do
       expect(store.phone_number).to eq("123-456-7890")
       expect(store.store_type).to eq("Nice Type")
     end
+
+    it "with different attrs" do
+      valid_attrs = {
+        longName: "Amy",
+        city: "Paris(Texas)",
+        distance: 1.34,
+        phone: "303-636-1111",
+        storeType: "Okay Type"
+      }
+      store = Store.new(valid_attrs)
+
+      expect(store.name).to eq("Amy")
+      expect(store.city).to eq("Paris(Texas)")
+      expect(store.distance).to eq(1.34)
+      expect(store.phone_number).to eq("303-636-1111")
+      expect(store.store_type).to eq("Okay Type")
+    end
   end
 end
